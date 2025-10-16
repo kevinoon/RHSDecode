ea5584bb36251153356a8421a63c85c1be8b198d
kevinoon <kev.tan000@rentonstudent.us>
2025-10-16 14:17:24 -0700

    Add EasyOpenCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.openftc.easyopencv.*;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvCameraFactory;
 
 @TeleOp(name = "Basic TeleOp", group = "Linear Opmode")
 public class BasicTeleOp extends LinearOpMode {
