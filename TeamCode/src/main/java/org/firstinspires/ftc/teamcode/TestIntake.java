440d15a16c98a46ea201d117024730ccdfa6e48e
kevinoon <kev.tan000@rentonstudent.us>
2025-10-28 15:35:35 -0700

    Add TestIntake.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestIntake.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.SubSystems.Intake;
+import org.firstinspires.ftc.teamcode.SubSystems.Shooter;
+import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
+@TeleOp(group = "A LeTeleOp", name = "IntakeTest")
+public class IntakeTester extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Intake intake = new Intake(hardwareMap);
+        GamepadEvents controller = new GamepadEvents(gamepad1);
+        waitForStart();
+        double power = 0.8;
+        while(opModeIsActive())
+        {
+            if(controller.dpad_up.onPress())
+            {
+                power += 0.1;
+            }
+            if(controller.dpad_down.onPress())
+            {
+                power -= 0.1;
+            }
+            intake.setPower(power);
+
+//            shooter.shoot(optimalRPM.getRPM(3.5));
+            telemetry.addData("Power: ", intake.getPower());
+            telemetry.update();
+            controller.update();
+        }
+    }
+}
