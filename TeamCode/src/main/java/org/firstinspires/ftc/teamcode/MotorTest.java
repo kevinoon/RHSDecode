114c9884469c6c0bca30f419133908c54e2815b4
kevinoon <kev.tan000@rentonstudent.us>
2025-10-30 17:39:01 -0700

    add stuff at end of 10/30/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -10,21 +10,30 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class MotorTest extends LinearOpMode {
     
     private DcMotor testMotor;
+    private Servo testServo;
+    private Servo testServo1;
 
     // public void init() {
-    //     testServo = hardwareMap.get(Servo.class, "testServo");
-    // }
+    //     testServo0 = hardwareMap.get(Servo.class, "testServo0");
+    //     testServo1 = hardwareMap.get(Servo.class, "testServo1");
+    //  }
     
     public void runOpMode() {
         
         testMotor = hardwareMap.get(DcMotor.class, "testMotor");
+        testServo = hardwareMap.get(Servo.class, "testServo");
+        testServo1 = hardwareMap.get(Servo.class, "testServo1");
         
         waitForStart();
         
         if(opModeIsActive()) {
             testMotor.setPower(1.0);
+            testServo.setPosition(1.0);
+            testServo1.setPosition(1.0);
             sleep(5000);
             testMotor.setPower(0);
+            testServo.setPosition(0);
+            testServo1.setPosition(0);
             sleep(5000);
 
             // These guys fr cant build anything what is the point of this, we have an hour left and they have made 0 progress on the robot
