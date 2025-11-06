88899b07e57129eff4ed367a9c739908ef7d83b9
Kevin Tang <kev.tan000@rentonstudent.us>
2025-11-06 14:44:58 -0800

    make a boolean comparison :sob:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTele.java
@@ -52,7 +52,7 @@ public class TestTele extends LinearOpMode {
             }
 
             // --- transLeft and TransRight Servos (gamepad2) ---
-            if (gamepad2.dpad_up > 0.05) {
+            if (gamepad2.dpad_up == true) {
                 hw.transLeft.setPosition(1.0);  // Adjust positions as needed
                 hw.transRight.setPosition(1.0);
             }
